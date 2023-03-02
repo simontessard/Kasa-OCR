1456764317a3c51d8f4257db4be8871262692c86
Simon Tessard <simon.tessard@gmail.com>
2023-03-02 12:35:18 +0100

    Moved logo + Footer CSS

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,10 @@
 import './footer.css'
+import logo from '../../assets/logo.svg'
 
 function Footer() {
   return (
     <footer>
+      <img src={logo} alt="Logo" />
       <p>© 2020 Kasa. All rights reserved</p>
     </footer>
   )
