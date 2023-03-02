199f5491f8f5baa56468db33b80888b277339fe9
Simon Tessard <simon.tessard@gmail.com>
2023-03-02 14:48:26 +0100

    Added logo and improved style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom'
 import './header.css'
+import logo from '../../assets/logo.svg'
 
 function Header() {
   return (
     <header>
+      <img src={logo} alt="Logo" />
       <nav>
         <Link to="/">Accueil</Link>
         <Link to="/about">À propos</Link>
