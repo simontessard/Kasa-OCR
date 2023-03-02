fb38e4bc197941b4367c1a5d1786571cf582f314
Simon Tessard <simon.tessard@gmail.com>
2023-03-02 18:48:30 +0100

    First cards + revamp of style with styled components

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,32 @@
 import image from '../../assets/sea.png'
 import './home.css'
+import Card from '../../components/Card/Card'
+import styled from 'styled-components'
+
+const HomeTitle = styled.h1`
+  color: white;
+  font-family: 'Montserrat';
+  font-weight: 500;
+  font-size: 48px;
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
 
 function Home() {
   return (
     <div className="homePage">
-      <img src={image} alt="Falaise" />
-      <h1> Chez vous, partout et ailleurs </h1>
+      <div className="homePage-title">
+        <img src={image} alt="Falaise" />
+        <HomeTitle> Chez vous, partout et ailleurs </HomeTitle>
+      </div>
+      <div className="cardContainer">
+        <Card title="Titre de la location" />
+        <Card title="Titre de la location" />
+        <Card title="Titre de la location" />
+      </div>
     </div>
   )
 }
