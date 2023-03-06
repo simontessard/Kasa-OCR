22aaf052a7651779066fd02809911e5bc8ef0d25
Simon Tessard <simon.tessard@gmail.com>
2023-03-06 14:55:02 +0100

    Improved Banner as a component to make it reusable

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,33 +1,38 @@
 import './about.css'
 import Collapse from '../../components/Collapse/Collapse'
+import Banner from '../../components/Banner/Banner'
+import image from '../../assets/sea.png'
 
 function About() {
   return (
-    <div className="about">
-      <Collapse
-        title="Fiabilité"
-        content="Les annonces postées sur Kasa garantissent 
+    <div>
+      <Banner image={image} />
+      <div className="about">
+        <Collapse
+          title="Fiabilité"
+          content="Les annonces postées sur Kasa garantissent 
       une fiabilité totale. Les photos sont conformes aux logements, et toutes les 
       informations sont régulièrement vérifiées  par nos équipes."
-      />
-      <Collapse
-        title="Respect"
-        content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
+        />
+        <Collapse
+          title="Respect"
+          content="La bienveillance fait partie des valeurs fondatrices de Kasa. 
         Tout comportement discriminatoire ou de perturbation 
         du voisinage entraînera une exclusion de notre plateforme."
-      />
-      <Collapse
-        title="Service"
-        content="Nos équipes se tiennent à votre disposition pour vous fournir une 
+        />
+        <Collapse
+          title="Service"
+          content="Nos équipes se tiennent à votre disposition pour vous fournir une 
         expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question."
-      />
-      <Collapse
-        title="Responsabilité "
-        content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
+        />
+        <Collapse
+          title="Responsabilité "
+          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, 
         chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note 
         aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés.
          Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-      />
+        />
+      </div>
     </div>
   )
 }
