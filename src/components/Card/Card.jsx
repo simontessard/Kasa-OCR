ccd3e7793814260ff4f0ce263419616f322a8773
Simon Tessard <simon.tessard@gmail.com>
2023-03-09 17:54:04 +0100

    Card style is matching the design model

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,6 +6,7 @@ function Card({ title, id }) {
   return (
     <NavLink to={'/location/' + id} className="card">
       <span>{title}</span>
+      <div className="card-filter"></div>
     </NavLink>
   )
 }
