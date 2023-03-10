8db97cf968f488182c54feb696b1bd5081ff9696
Simon Tessard <simon.tessard@gmail.com>
2023-03-10 12:21:14 +0100

    Moved router file

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-import Home from '../../pages/Home/Home'
-import Location from '../../pages/Location/Location'
-import Header from '../Header/Header'
-import Footer from '../Footer/Footer'
-import Error from '../Error/Error'
-import About from '../../pages/About/About'
+import Home from './pages/Home/Home'
+import Location from './pages/Location/Location'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Error from './components/Error/Error'
+import About from './pages/About/About'
 
 const Routing = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Routing from './components/Router/Router';
+import Routing from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
